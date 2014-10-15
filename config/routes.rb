c6ad11eea1c3e09c70f34dba120f3c2f9ff6865d
David Fisher <tibbon@gmail.com>
2014-10-15 15:24:47 -0400

    Updated example. Setup devise initilizer. Setup routes for omniauth callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#show'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
