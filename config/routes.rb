e288dd8b9a502167547e088d437093405f67b766
David Fisher <tibbon@gmail.com>
2014-10-15 15:31:16 -0400

    Added name column to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   root 'home#show'
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  devise_scope :user do
-    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  end
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
